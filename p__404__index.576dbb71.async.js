c1a3cf3fc1b19f3db63e7dc29011696391ede698
rensiwei <vsion.ren@tenxcloud.com>
2023-05-02 06:27:54 +0800

    Updates

--- p__404__index.576dbb71.async.js
+++ p__404__index.576dbb71.async.js
@@ -1,7 +1,7 @@
 /*!
  * @Licensed Materials - Property of xxx.com
  * (C) Copyright 2022~2023 xxx.com. All Rights Reserved.
- * @date 1682978648009
+ * @date 1682979029026
  * http://xxx.com
  */
 "use strict";(self.webpackChunkvsion_liusisi=self.webpackChunkvsion_liusisi||[]).push([[333],{66837:function(s,i,n){n.r(i),n.d(i,{default:function(){return o},duration:function(){return d}});n(50959);var e=n(57288),r="wrapper404___m0r6N",c="content___flcUu",t="img___IrALp",a="text___jEVkL",l="btn___C3a63",u=n(98025),_=n(11527),d=200,o=function(s){return(0,_.jsx)("div",{className:r,children:(0,_.jsxs)("div",{className:c,children:[(0,_.jsx)("div",{className:t}),(0,_.jsx)("div",{className:a,children:"抱歉，你访问的页面不存在"}),(0,_.jsx)("div",{className:l,children:(0,_.jsx)(e.ZP,{size:"large",type:"primary",ghost:!0,onClick:function(){u.history.push("/")},children:"返回首页"})})]})})}}}]);
